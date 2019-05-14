7a1b01e818574c71c1c541984b4e0773c9467722
Jasmine Humbert <jasmine.yhumbert@gmail.com>
2019-05-14 16:05:20 -0700

    [refactor] added comments and docstrings

--- routes/models.py
+++ routes/models.py
@@ -4,14 +4,17 @@ from .data_reader import routes_gen
 # Create your models here.
 
 class Price(models.Model):
+    """Route and its cost"""
     number = models.CharField(max_length=17)
     price = models.CharField(max_length=6)
 
 
 class CarrierList(models.Model):
+    """Text file with carrier route prices"""
     data = models.FileField()
 
     def save(self, *args, **kwargs):
+        """Adds carrier route prices to db whenever a new carrier list is uploaded"""
         super(CarrierList, self).save(*args, **kwargs)
         filename = self.data.url
         prices = []
@@ -25,13 +28,13 @@ class CarrierList(models.Model):
         Price.objects.bulk_create(prices)
 
 def get_price(number):
+    """Fetches the most specific matching route cost of a number and returns the cheapest option if multiple are found"""
     print("getting price")
     prefix = number
     while prefix != "":
         print("checking prefix:", prefix)
         try:
             price = Price.objects.get(number=prefix).price
-            print("found price:", price)
             return str(price)
         except Price.DoesNotExist:
             prefix = prefix[:-1]
@@ -41,5 +44,4 @@ def get_price(number):
 
             return min(prices)
     
-    print("could not find an entry :(")
-    return "No route found."
+    return "0"
